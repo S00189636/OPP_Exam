e151ec35f9e755c5c6e7dfe8166efbcb9c4b6e73
mohamed shiha <46888852+S00189636@users.noreply.github.com>
2019-12-11 09:48:17 +0000

    Player class created

---
+++ TeamApp/Exam/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    public class Player
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public int Age { get { return DateTime.Now.Subtract(DOB).Days / 365; } }
+        public Position PreferredPosition { get; set; }
+    }
+}

59c9b63624a6f6ad7655b218e3b1dc2f56f38427
mohamed shiha <46888852+S00189636@users.noreply.github.com>
2019-12-11 10:53:40 +0000

    adding and removing

--- TeamApp/Exam/MainWindow.xaml.cs
+++ TeamApp/Exam/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace Exam
         // used in creating random player
         ObservableCollection<Player> _allPlayers;
         ObservableCollection<Player> _selectedPlayers;
+        string[] comboArray = { "4-4-2", "4-3-3", "4-5-1" };
+        int _spaces = 11;
         Random random = new Random();
         public MainWindow()
         {
@@ -37,16 +39,45 @@ namespace Exam
             // when the main window is loadded creat the players
             CreatPlayers();
             lstbAllPlayers.ItemsSource = _allPlayers;
+            lstbSelectedPlayers.ItemsSource = _selectedPlayers;
+            cboxFormating.ItemsSource = comboArray;
+            cboxFormating.SelectedIndex = 0;
         }
 
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
 
+            Player playerToAdd = lstbAllPlayers.SelectedItem as Player;
+            // if nothing is selectd do not add anything 
+            if (playerToAdd == null) return;
+            // if no spaces or the selected player is not valid show the player
+            if(_spaces == 0)
+            {
+                MessageBox.Show("No more spaces", "Adding Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }else if (!AddToSelected(playerToAdd))
+            {
+                MessageBox.Show($"You already have {playerToAdd.PreferredPosition}", "Adding Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            _allPlayers.Remove(playerToAdd);
+            // when i add a player space should be decreased
+            _spaces--;
+            // everytime i add or remove the list are resorted 
+            UpdateUIElements();
         }
 
         private void BtnRemove_Click(object sender, RoutedEventArgs e)
         {
-
+            Player playerToRemove = lstbSelectedPlayers.SelectedItem as Player;
+            if (playerToRemove == null) return;
+            _selectedPlayers.Remove(playerToRemove);
+            _allPlayers.Add(playerToRemove);
+            // when i remove a player spaceshould be increased
+            _spaces++;
+            // everytime i add or remove the list are resorted 
+            UpdateUIElements();
         }
 
         // will use the position to creat a new player
@@ -76,6 +107,66 @@ namespace Exam
                 _allPlayers.Add(CreatRandomPlayer(Position.Defender));
             for (int i = 0; i < 4; i++)
                 _allPlayers.Add(CreatRandomPlayer(Position.Forward));
+
+        }
+
+
+        // this will check if the selected player is a valid selection then add it to the list 
+        private bool AddToSelected(Player player)
+        {
+            // get the formation
+            string[] selectedFormation = (cboxFormating.SelectedItem as string).Split('-');
+            // get the formation in ints
+            int defenders = int.Parse(selectedFormation[0]);
+            int Midfielders = int.Parse(selectedFormation[1]);
+            int Forwarders = int.Parse(selectedFormation[2]);
+            int count;
+            switch (player.PreferredPosition)
+            {
+                case Position.Goalkeeper:
+                    count = 1;
+                    foreach (Player sPlayer in _selectedPlayers)
+                        if (sPlayer.PreferredPosition == Position.Goalkeeper) count--;
+                    if (count == 0) return false;
+                    break;
+                case Position.Defender:
+                    count = defenders;
+                    foreach (Player sPlayer in _selectedPlayers)
+                    {
+                        if (sPlayer.PreferredPosition == Position.Defender)
+                            count--;
+                        if (count == 0) return false;
+                    }
+                    break;
+                case Position.Midfielder:
+                    count = Midfielders;
+                    foreach (Player sPlayer in _selectedPlayers)
+                    {
+                        if (sPlayer.PreferredPosition == Position.Midfielder)
+                            count--;
+                        if (count == 0) return false;
+                    }
+                    break;
+                case Position.Forward:
+                    count = Forwarders;
+                    foreach (Player sPlayer in _selectedPlayers)
+                    {
+                        if (sPlayer.PreferredPosition == Position.Forward)
+                            count--;
+                        if (count == 0) return false;
+                    }
+                    break;
+            }
+            _selectedPlayers.Add(player);
+            return true;
+        }
+
+        // will be called after updates 
+        private void UpdateUIElements()
+        {
+            _allPlayers.Sort();
+            _selectedPlayers.Sort();
+            txtbSpaces.Text = _spaces.ToString();
         }
     }
 }
